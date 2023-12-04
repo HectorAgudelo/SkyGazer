23cba736cb65cc937da1c8de65474b5e2623e2a7
Hector Agudelo <haacny86@gmail.com>
2023-12-04 12:00:43 -0500

    adding axios and api request to NASA and NEWSAPI

--- SkyGazer/src/App.jsx
+++ SkyGazer/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+ import {getAPOD} from './APIs/NasaApi'
+import getNews from './APIs/NewsAPI' 
 
 function App() {
 
+getAPOD().then (res => console.log(res))
+
+getNews().then (res => console.log(res))
 
   return (
     <>
