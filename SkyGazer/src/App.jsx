1d4e48055befc0953de133f24c8d7976f837101c
Hector Agudelo <haacny86@gmail.com>
2023-12-10 02:11:48 -0500

    function to fetch data from firebase

--- SkyGazer/src/App.jsx
+++ SkyGazer/src/App.jsx
@@ -3,11 +3,14 @@ import { getAPOD } from "./APIs/NasaApi";
 import getNews from "./APIs/NewsAPI";
 import ArticleCard from "./components/ArticleCard";
 import dummyArticles from "./dummyData";
+import fetchDataFromFirebase from "./APIs/fireBase";
 
 function App() {
   // getAPOD().then (res => console.log(res))
 
   // getNews().then (res => console.log(res))
+
+  fetchDataFromFirebase().then((res) => console.log(res));  
 
   return (
     <>
